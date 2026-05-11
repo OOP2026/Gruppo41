ce33ff87304fb2d33fe87b7c07e4c2478a09944f
SimoG77 <simone.guida@studenti.unina.it>
2026-05-11 21:43:07 +0200

    Add files via upload

---
+++ src/main/java/Docente.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.List;
+
+    public class Docente extends Utente {
+    private String ruolo;
+    private List<Vincolo> vincoli;
+    private List<Lezione> lezioni;
+
+    public Docente(String nome, String cognome, String email, String password, String ruolo) {
+        super(nome, cognome, email, password);
+        this.ruolo = ruolo;
+    }
+
+    public void visualizzaOrario() {
+       //orario
+    }
+    public void richiestaSpostamentoLezione(SpostamentoLezione s) {}
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+
+    public List<Vincolo> getVincoli() {
+        return vincoli;
+    }
+    public void setVincoli(List<Vincolo> vincoli) {
+        this.vincoli = vincoli;
+    }
+
+    public List<Lezione> getLezioni() {
+        return lezioni;
+    }
+    public void setLezioni(List<Lezione> lezioni) {
+        this.lezioni = lezioni;
+    }
+}
