9760e23464e8757822d72f02f550dc199957ca8a
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-09 16:30:49 +0200

    Update Docente.java

--- src/main/java/model/Docente.java
+++ src/main/java/model/Docente.java
@@ -4,42 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Docente extends Utente {
-    private String ruolo;
-    private List<Lezione> lezioni;
+    private List<Insegnamento> insegnamenti;
     private List<Vincolo> vincoli;
 
-    public Docente(String nome, String cognome, String email, String password, String ruolo) {
-        super(nome, cognome, email, password);
-        this.ruolo = ruolo;
-        this.lezioni = new ArrayList<>();
+    public Docente(String nome, String cognome, String email, String login, String password) {
+        super(nome, cognome, email, login, password);
+        this.insegnamenti = new ArrayList<>();
         this.vincoli = new ArrayList<>();
     }
 
-    public String getRuolo() {
-        return ruolo;
-    }
+    public List<Insegnamento> getInsegnamenti() { return insegnamenti; }
+    public void setInsegnamenti(List<Insegnamento> insegnamenti) { this.insegnamenti = insegnamenti; }
+    public List<Vincolo> getVincoli() { return vincoli; }
+    public void setVincoli(List<Vincolo> vincoli) { this.vincoli = vincoli; }
 
-    public List<Lezione> getLezioni() {
-        return lezioni;
-    }
-
-    public void visualizzaOrario() {
-        System.out.println("Visualizzazione orario docente...");
-    }
-
-    public void setLezioni(List<Lezione> lezioni) {
-        this.lezioni = lezioni;
-    }
-
-    public List<Vincolo> getVincoli() {
-        return vincoli;
-    }
-
-    public void setVincoli(List<Vincolo> vincoli) {
-        this.vincoli = vincoli;
-    }
-
-    public void richiestaSpostamentoLezione(SpostamentoLezione spostamento) {
-        // Metodo stub per i test
+    public boolean aggiungiVincolo(Vincolo v) {
+        if (this.vincoli.size() >= 3) {
+            return false;
+        }
+        this.vincoli.add(v);
+        return true;
     }
 }
