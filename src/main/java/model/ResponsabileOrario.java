a521bd1587304b109a54416e736fcc43d6293150
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-09 16:31:14 +0200

    Update ResponsabileOrario.java

--- src/main/java/model/ResponsabileOrario.java
+++ src/main/java/model/ResponsabileOrario.java
@@ -1,20 +1,7 @@
 package model;
 
 public class ResponsabileOrario extends Docente {
-
-    public ResponsabileOrario(String nome, String cognome, String email, String password, String ruolo) {
-        super(nome, cognome, email, password, ruolo);
-    }
-
-    public void creazioneLezione(Lezione l) {
-        // Metodo stub per i test
-    }
-
-    public void inserisciAulaDisponibile(Aula a) {
-        // Metodo stub per i test
-    }
-
-    public void visualizzaEventualiConflitti() {
-        // Metodo stub per i test
+    public ResponsabileOrario(String nome, String cognome, String email, String login, String password) {
+        super(nome, cognome, email, login, password);
     }
 }
