713bf213fd3bee42adb189bb50d814378d8d62e8
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-09 16:29:46 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,36 +1,43 @@
 package model;
 
+import java.util.Objects;
+
 public class Utente {
+    private String nome;
+    private String cognome;
+    private String email;
+    private String login;
+    private String password;
 
-    protected String nome;
-    protected String cognome;
-    protected String email;
-    protected String password;
-
-    public Utente(String nome,
-                  String cognome,
-                  String email,
-                  String password) {
-
+    public Utente(String nome, String cognome, String email, String login, String password) {
         this.nome = nome;
         this.cognome = cognome;
         this.email = email;
+        this.login = login;
         this.password = password;
     }
 
-    public String getNome() {
-        return nome;
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCognome() { return cognome; }
+    public void setCognome(String cognome) { this.cognome = cognome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Utente utente = (Utente) o;
+        return Objects.equals(login, utente.login);
     }
 
-    public String getCognome() {
-        return cognome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
     }
 }
