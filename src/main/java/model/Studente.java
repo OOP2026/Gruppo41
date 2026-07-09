3b0ffaf04dabf0becf4ec0984b358a96f3cfd8e4
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-09 16:30:22 +0200

    Update Studente.java

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -1,38 +1,13 @@
 package model;
 
-import java.time.LocalTime;
-import java.util.List;
-import java.util.ArrayList;
-
-    public class Studente extends Utente {
+public class Studente extends Utente {
     private String matricola;
-    private List<Insegnamento> insegnamenti;
 
-    public Studente(String nome, String cognome, String email, String password, String matricola) {
-        super(nome, cognome, email, password);
+    public Studente(String nome, String cognome, String email, String login, String password, String matricola) {
+        super(nome, cognome, email, login, password);
         this.matricola = matricola;
     }
 
-    public void visualizzaOrarioAnnoDiCorso() {
-        //metodo da implementare
-    }
-    public void visualizzaAula() {
-        //metodo da implementare
-    }
-
-    public String getMatricola() {
-        return matricola;
-    }
-    public void setMatricola(String matricola) {
-
-        this.matricola = matricola;
-    }
-
-    public List<Insegnamento> getInsegnamenti() {
-
-        return insegnamenti;
-    }
-    public void setInsegnamenti(List<Insegnamento> insegnamenti) {
-        this.insegnamenti = insegnamenti;
-    }
+    public String getMatricola() { return matricola; }
+    public void setMatricola(String matricola) { this.matricola = matricola; }
 }
