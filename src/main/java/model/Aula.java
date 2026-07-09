053b97df69304c3b44412f3749a12649ab275c35
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-09 16:31:58 +0200

    Update Aula.java

--- src/main/java/model/Aula.java
+++ src/main/java/model/Aula.java
@@ -1,31 +1,27 @@
 package model;
 
-import java.time.LocalTime;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Objects;
 
-    public class Aula {
+public class Aula {
     private String nome;
-    private List<Lezione> lezioni;
 
     public Aula(String nome) {
         this.nome = nome;
-        this.lezioni = new ArrayList<>();
     }
 
-    public String getNome() {
-        return nome;
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aula aula = (Aula) o;
+        return Objects.equals(nome, aula.nome);
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Lezione> getLezioni() {
-        return lezioni;
-    }
-
-    public void setLezioni(List<Lezione> lezioni) {
-        this.lezioni = lezioni;
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
     }
 }
