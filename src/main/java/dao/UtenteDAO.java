267db455b6a0abdc51526197af399968f782e0e0
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-09 16:48:45 +0200

    Update UtenteDAO.java

--- src/main/java/dao/UtenteDAO.java
+++ src/main/java/dao/UtenteDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
-public interface UtenteDAO {
+import model.Utente;
+import model.Studente;
+import model.Docente;
 
+public interface UtenteDAO {
+    Utente login(String login, String password);
+    boolean registraStudente(Studente studente);
+    boolean registraDocente(Docente docente);
 }
