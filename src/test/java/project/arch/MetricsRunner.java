2a87f6e5549665fc962678bec7cf4490876dba27
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-10 15:41:52 +0200

    Update MetricsRunner.java

--- src/test/java/project/arch/MetricsRunner.java
+++ src/test/java/project/arch/MetricsRunner.java
@@ -3,9 +3,9 @@ package project.arch;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.library.metrics.ArchitectureMetrics;
-import com.tngtech.archunit.library.metrics.MetricsComponents;
 import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MetricsRunner {
 
@@ -13,10 +13,16 @@ public class MetricsRunner {
     public void calculateMetrics() {
         JavaClasses classes = new ClassFileImporter().importPackages("controller", "dao", "implementazioneDao", "model", "gui");
         
-        MetricsComponents<JavaClass> components = MetricsComponents.from(classes);
-        
         int totalClasses = classes.size();
+        Set<String> packages = new HashSet<>();
+        
+        for (JavaClass javaClass : classes) {
+            packages.add(javaClass.getPackageName());
+        }
+        
         System.out.println("=== METRICHE DI PROGETTO ===");
         System.out.println("Numero totale di classi analizzate: " + totalClasses);
+        System.out.println("Numero totale di pacchetti analizzati: " + packages.size());
+        System.out.println("Pacchetti rilevati: " + packages);
     }
 }
