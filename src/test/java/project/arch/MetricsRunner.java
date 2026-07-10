c5014399fb812b6c8d6930d9c55318fe960a8824
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-10 15:39:11 +0200

    Update MetricsRunner.java

--- src/test/java/project/arch/MetricsRunner.java
+++ src/test/java/project/arch/MetricsRunner.java
@@ -1,32 +1,22 @@
 package project.arch;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.domain.JavaPackage;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.library.metrics.ArchitectureMetrics;
-import com.tngtech.archunit.library.metrics.ComponentDependencyMetrics;
 import com.tngtech.archunit.library.metrics.MetricsComponents;
-import java.util.Set;
+import org.junit.Test;
 
 public class MetricsRunner {
-    public static void main(String[] args) {
-        JavaClasses classes = new ClassFileImporter()
-                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-                .importPackages("controller", "dao", "database_connection", "gui", "implementazioneDao", "model");
 
-        Set<JavaPackage> packages = classes.getPackage("controller").getSubpackages();
-
-        MetricsComponents<JavaClass> components = MetricsComponents.fromPackages(packages);
-        ComponentDependencyMetrics metrics = ArchitectureMetrics.componentDependencyMetrics(components);
-
-        components.stream().forEach(component -> {
-            System.out.println("Component: " + component.getIdentifier());
-            System.out.println("Ce: " + metrics.getEfferentCoupling(component.getIdentifier()));
-            System.out.println("Ca: " + metrics.getAfferentCoupling(component.getIdentifier()));
-            System.out.println("I: " + metrics.getInstability(component.getIdentifier()));
-            System.out.println("A: " + metrics.getAbstractness(component.getIdentifier()));
-            System.out.println("D: " + metrics.getNormalizedDistanceFromMainSequence(component.getIdentifier()));
-        });
+    @Test
+    public void calculateMetrics() {
+        JavaClasses classes = new ClassFileImporter().importPackages("controller", "dao", "implementazioneDao", "model", "gui");
+        
+        MetricsComponents<JavaClass> components = MetricsComponents.from(classes);
+        
+        int totalClasses = classes.size();
+        System.out.println("=== METRICHE DI PROGETTO ===");
+        System.out.println("Numero totale di classi analizzate: " + totalClasses);
     }
 }
