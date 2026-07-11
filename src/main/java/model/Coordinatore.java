f679fb819128475d6b80b63ae47849c40bedfa45
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-11 18:24:51 +0200

    Update Coordinatore.java

--- src/main/java/model/Coordinatore.java
+++ src/main/java/model/Coordinatore.java
@@ -1,6 +1,7 @@
 package model;
 
 public class Coordinatore extends ResponsabileOrario {
+
     public Coordinatore(String nome, String cognome, String email, String login, String password) {
         super(nome, cognome, email, login, password);
     }
