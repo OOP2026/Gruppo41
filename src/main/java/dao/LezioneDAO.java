8e34c569126a7228b9739128b4db7d29a51b7c4e
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-11 18:42:34 +0200

    Update LezioneDAO.java

--- src/main/java/dao/LezioneDAO.java
+++ src/main/java/dao/LezioneDAO.java
@@ -1,21 +1,35 @@
 package dao;
 
-import model.Lezione;
 import model.Aula;
 import model.Insegnamento;
+import model.Lezione;
 import model.SpostamentoLezione;
 import model.Vincolo;
+
 import java.util.List;
 
 public interface LezioneDAO {
     List<Lezione> getTutteLeLezioni();
+
     List<Lezione> getLezioniPerAnno(String annoCorso);
+
     List<Lezione> getLezioniPerDocente(String loginDocente);
+
     boolean inserisciLezione(Lezione lezione);
+
     List<Aula> getAuleDisponibili();
+
+    boolean inserisciAula(Aula aula);
+
     List<Insegnamento> getInsegnamentiAttivi();
+
+    boolean inserisciInsegnamento(Insegnamento insegnamento);
+
     boolean inserisciVincolo(String loginDocente, Vincolo vincolo);
+
     boolean richiediSpostamento(SpostamentoLezione spostamento);
+
     List<SpostamentoLezione> getRichiesteSpostamento();
+
     boolean aggiornaStatoSpostamento(SpostamentoLezione spostamento, String stato);
 }
