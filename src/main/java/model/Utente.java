0154d9b0121e5a7217a66d0b9e13eeef2fe5ad4e
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-11 18:21:32 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -3,11 +3,12 @@ package model;
 import java.util.Objects;
 
 public class Utente {
-    private String nome;
-    private String cognome;
-    private String email;
-    private String login;
-    private String password;
+
+    protected String nome;
+    protected String cognome;
+    protected String email;
+    protected String login;
+    protected String password;
 
     public Utente(String nome, String cognome, String email, String login, String password) {
         this.nome = nome;
@@ -17,21 +18,54 @@ public class Utente {
         this.password = password;
     }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public String getCognome() { return cognome; }
-    public void setCognome(String cognome) { this.cognome = cognome; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getLogin() { return login; }
-    public void setLogin(String login) { this.login = login; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Utente)) {
+            return false;
+        }
         Utente utente = (Utente) o;
         return Objects.equals(login, utente.login);
     }
