f6a8ab6ff0eb8f746493afe0ef38bcaf43e7e99e
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-11 18:26:05 +0200

    Update Insegnamento.java

--- src/main/java/model/Insegnamento.java
+++ src/main/java/model/Insegnamento.java
@@ -1,6 +1,7 @@
 package model;
 
 public class Insegnamento {
+
     private String nome;
     private int cfu;
     private String annoCorso;
@@ -13,12 +14,40 @@ public class Insegnamento {
         this.docente = docente;
     }
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public int getCfu() { return cfu; }
-    public void setCfu(int cfu) { this.cfu = cfu; }
-    public String getAnnoCorso() { return annoCorso; }
-    public void setAnnoCorso(String annoCorso) { this.annoCorso = annoCorso; }
-    public Docente getDocente() { return docente; }
-    public void setDocente(Docente docente) { this.docente = docente; }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCfu() {
+        return cfu;
+    }
+
+    public void setCfu(int cfu) {
+        this.cfu = cfu;
+    }
+
+    public String getAnnoCorso() {
+        return annoCorso;
+    }
+
+    public void setAnnoCorso(String annoCorso) {
+        this.annoCorso = annoCorso;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (anno " + annoCorso + ", " + docente.getNome() + " " + docente.getCognome() + ")";
+    }
 }
