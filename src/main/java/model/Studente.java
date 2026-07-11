caa355e32d13702825f308a191e1a6fe84e16c69
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-11 18:22:09 +0200

    Update Studente.java

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -1,13 +1,30 @@
 package model;
 
 public class Studente extends Utente {
-    private String matricola;
 
-    public Studente(String nome, String cognome, String email, String login, String password, String matricola) {
+    private String matricola;
+    private String annoCorso;
+
+    public Studente(String nome, String cognome, String email, String login, String password,
+                     String matricola, String annoCorso) {
         super(nome, cognome, email, login, password);
+        this.matricola = matricola;
+        this.annoCorso = annoCorso;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
         this.matricola = matricola;
     }
 
-    public String getMatricola() { return matricola; }
-    public void setMatricola(String matricola) { this.matricola = matricola; }
+    public String getAnnoCorso() {
+        return annoCorso;
+    }
+
+    public void setAnnoCorso(String annoCorso) {
+        this.annoCorso = annoCorso;
+    }
 }
