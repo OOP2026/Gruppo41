5c13696a75a1011106620390e81a592da6dfbd26
raffaele018485 <raffae.lucci@studenti.unina.it>
2026-07-12 22:10:11 +0200

    Update Controller.java

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -17,138 +17,138 @@ import java.util.List;
 
 public class Controller {
 
-    private List<Lezione> lezioni;[cite: 7]
-    private final UtenteDAO utenteDao;[cite: 7]
-    private final LezioneDAO lezioneDao;[cite: 7]
-    private Utente utenteLoggato;[cite: 7]
-    private AppLauncher launcher;[cite: 7]
+    private List<Lezione> lezioni;
+    private final UtenteDAO utenteDao;
+    private final LezioneDAO lezioneDao;
+    private Utente utenteLoggato;
+    private AppLauncher launcher;
 
     public Controller() {
-        this.utenteDao = new UtentePostgresDAO();[cite: 7]
-        this.lezioneDao = new LezionePostgresDAO();[cite: 7]
-        this.lezioni = new ArrayList<>();[cite: 7]
+        this.utenteDao = new UtentePostgresDAO();
+        this.lezioneDao = new LezionePostgresDAO();
+        this.lezioni = new ArrayList<>();
     }
 
     public void setLauncher(AppLauncher launcher) {
-        this.launcher = launcher;[cite: 7]
+        this.launcher = launcher;
     }
 
     public void avviaApplicazione() {
-        if (launcher != null) {[cite: 7]
-            launcher.launch();[cite: 7]
+        if (launcher != null) {
+            launcher.launch();
         }
     }
 
     public boolean login(String username, String password) {
-        Utente u = utenteDao.login(username, password);[cite: 7]
-        if (u != null) {[cite: 7]
-            this.utenteLoggato = u;[cite: 7]
-            return true;[cite: 7]
+        Utente u = utenteDao.login(username, password);
+        if (u != null) {
+            this.utenteLoggato = u;
+            return true;
         }
-        return false;[cite: 7]
+        return false;
     }
 
     public Utente getUtenteLoggato() {
-        return utenteLoggato;[cite: 7]
+        return utenteLoggato;
     }
 
     public void logout() {
-        this.utenteLoggato = null;[cite: 7]
+        this.utenteLoggato = null;
     }
 
     public List<Lezione> getLezioni() {
         try {
-            this.lezioni = lezioneDao.getTutteLeLezioni();[cite: 7]
+            this.lezioni = lezioneDao.getTutteLeLezioni();
         } catch (Exception e) {
             throw new RuntimeException("Errore durante il recupero delle lezioni dal database", e);
         }
-        return lezioni;[cite: 7]
+        return lezioni;
     }
 
     public List<Lezione> getLezioniPerAnno(String anno) {
-        return lezioneDao.getLezioniPerAnno(anno);[cite: 7]
+        return lezioneDao.getLezioniPerAnno(anno);
     }
 
     public List<Lezione> getLezioniPerDocente(String login) {
-        return lezioneDao.getLezioniPerDocente(login);[cite: 7]
+        return lezioneDao.getLezioniPerDocente(login);
     }
 
     public List<Aula> getAuleDisponibili() {
-        return lezioneDao.getAuleDisponibili();[cite: 7]
+        return lezioneDao.getAuleDisponibili();
     }
 
     public boolean inserisciAula(Aula aula) {
-        return lezioneDao.inserisciAula(aula);[cite: 7]
+        return lezioneDao.inserisciAula(aula);
     }
 
     public List<Insegnamento> getInsegnamentiAttivi() {
-        return lezioneDao.getInsegnamentiAttivi();[cite: 7]
+        return lezioneDao.getInsegnamentiAttivi();
     }
 
     public boolean inserisciInsegnamento(Insegnamento insegnamento) {
-        return lezioneDao.inserisciInsegnamento(insegnamento);[cite: 7]
+        return lezioneDao.inserisciInsegnamento(insegnamento);
     }
 
     public boolean aggiungiLezione(Lezione lezione) {
-        if (verificaConflitti(lezione) && verificaVincoliDocente(lezione)) {[cite: 7]
-            return lezioneDao.inserisciLezione(lezione);[cite: 7]
+        if (verificaConflitti(lezione) && verificaVincoliDocente(lezione)) {
+            return lezioneDao.inserisciLezione(lezione);
         }
-        return false;[cite: 7]
+        return false;
     }
 
     private boolean verificaConflitti(Lezione nuova) {
-        List<Lezione> esistenti = getLezioni();[cite: 7]
-        for (Lezione l : esistenti) {[cite: 7]
-            if (l.getGiornoSettimana().equalsIgnoreCase(nuova.getGiornoSettimana())) {[cite: 7]
-                boolean sovrapposizioneOraria = nuova.getOraInizio().isBefore(l.getOraFine())[cite: 7]
-                        && nuova.getOraFine().isAfter(l.getOraInizio());[cite: 7]
-                if (sovrapposizioneOraria) {[cite: 7]
-                    if (l.getAula().getNome().equalsIgnoreCase(nuova.getAula().getNome())) {[cite: 7]
-                        return false;[cite: 7]
+        List<Lezione> esistenti = getLezioni();
+        for (Lezione l : esistenti) {
+            if (l.getGiornoSettimana().equalsIgnoreCase(nuova.getGiornoSettimana())) {
+                boolean sovrapposizioneOraria = nuova.getOraInizio().isBefore(l.getOraFine())
+                        && nuova.getOraFine().isAfter(l.getOraInizio());
+                if (sovrapposizioneOraria) {
+                    if (l.getAula().getNome().equalsIgnoreCase(nuova.getAula().getNome())) {
+                        return false;
                     }
-                    if (l.getInsegnamento().getDocente().getLogin()[cite: 7]
-                            .equalsIgnoreCase(nuova.getInsegnamento().getDocente().getLogin())) {[cite: 7]
-                        return false;[cite: 7]
+                    if (l.getInsegnamento().getDocente().getLogin()
+                            .equalsIgnoreCase(nuova.getInsegnamento().getDocente().getLogin())) {
+                        return false;
                     }
                 }
             }
         }
-        return true;[cite: 7]
+        return true;
     }
 
     private boolean verificaVincoliDocente(Lezione nuova) {
-        Docente docente = nuova.getInsegnamento().getDocente();[cite: 7]
-        if (docente == null) {[cite: 7]
-            return true;[cite: 7]
+        Docente docente = nuova.getInsegnamento().getDocente();
+        if (docente == null) {
+            return true;
         }
-        for (Vincolo v : docente.getVincoli()) {[cite: 7]
-            if (v.getGiorno().equalsIgnoreCase(nuova.getGiornoSettimana())) {[cite: 7]
-                boolean sovrapposizione = nuova.getOraInizio().isBefore(v.getOraFine())[cite: 7]
-                        && nuova.getOraFine().isAfter(v.getOraInizio());[cite: 7]
-                if (sovrapposizione) {[cite: 7]
-                    return false;[cite: 7]
+        for (Vincolo v : docente.getVincoli()) {
+            if (v.getGiorno().equalsIgnoreCase(nuova.getGiornoSettimana())) {
+                boolean sovrapposizione = nuova.getOraInizio().isBefore(v.getOraFine())
+                        && nuova.getOraFine().isAfter(v.getOraInizio());
+                if (sovrapposizione) {
+                    return false;
                 }
             }
         }
-        return true;[cite: 7]
+        return true;
     }
 
     public boolean inserisciVincolo(Vincolo v) {
-        if (utenteLoggato instanceof Docente) {[cite: 7]
-            return lezioneDao.inserisciVincolo(utenteLoggato.getLogin(), v);[cite: 7]
+        if (utenteLoggato instanceof Docente) {
+            return lezioneDao.inserisciVincolo(utenteLoggato.getLogin(), v);
         }
-        return false;[cite: 7]
+        return false;
     }
 
     public boolean richiediSpostamento(SpostamentoLezione spostamento) {
-        return lezioneDao.richiediSpostamento(spostamento);[cite: 7]
+        return lezioneDao.richiediSpostamento(spostamento);
     }
 
     public List<SpostamentoLezione> getRichiesteSpostamento() {
-        return lezioneDao.getRichiesteSpostamento();[cite: 7]
+        return lezioneDao.getRichiesteSpostamento();
     }
 
     public boolean aggiornaStatoSpostamento(SpostamentoLezione s, String stato) {
-        return lezioneDao.aggiornaStatoSpostamento(s, stato);[cite: 7]
+        return lezioneDao.aggiornaStatoSpostamento(s, stato);
     }
 }
